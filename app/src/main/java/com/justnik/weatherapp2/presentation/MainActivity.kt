8d950ac23435cb9737a23780628765bbcc6c53e8
JustNik8 <justcrazynik@gmail.com>
2022-01-29 21:27:31 +0300

    First Setup

--- app/src/main/java/com/justnik/weatherapp2/presentation/MainActivity.kt
+++ app/src/main/java/com/justnik/weatherapp2/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.justnik.weatherapp2
+package com.justnik.weatherapp2.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.justnik.weatherapp2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
